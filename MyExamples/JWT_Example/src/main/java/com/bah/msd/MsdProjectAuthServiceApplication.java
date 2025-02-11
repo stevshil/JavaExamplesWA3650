7d3aa8ea2231df91fcb35a048e70328c5e631f42
Steve Shilling <stevshil@users.noreply.github.com>
2025-02-11 14:41:39 +0000

    JWT Example

---
+++ MyExamples/JWT_Example/src/main/java/com/bah/msd/MsdProjectAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdProjectAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdProjectAuthServiceApplication.class, args);
+	}
+
+}
